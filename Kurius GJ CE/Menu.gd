eed660592424dd13856b1aa8459501125f1472f3
joomeu <82919096+joomeu@users.noreply.github.com>
2021-12-30 01:22:55 -0500

    Add files via upload

--- Kurius GJ CE/Menu.gd
+++ Kurius GJ CE/Menu.gd
@@ -8,12 +8,32 @@ func _ready():
 
 
 func _on_StartButton_pressed():
-	$ButtonClickedSound.play()
+	$StartSound.play()
 	$transition.play("fade")
 	$fade_bgm.play("fade_out")
 	
-	if $ButtonClickedSound.playing:
-		yield($ButtonClickedSound, "finished")
+	if $StartSound.playing:
+		yield($StartSound, "finished")
 		
 	get_tree().change_scene("res://cutscenes/Opening_scene/Opening_scene.tscn")
 	
+func _on_AboutButton_pressed():
+	$CreditsSound.play()
+	$transition.play("fade")
+	$bg_credits.visible = true
+	$CreditsHbox.visible = true
+	$CreditsHbox/BackButton.visible = true
+	$CreditsHbox/BackButtonText.visible = true
+	$CreditsText.visible = true
+	$transition.play("fade_out")
+
+
+func _on_BackButton_pressed():
+	$CreditsSound.play()
+	$transition.play("fade")
+	$bg_credits.visible = false
+	$CreditsHbox.visible = false
+	$CreditsHbox/BackButton.visible = false
+	$CreditsHbox/BackButtonText.visible = false
+	$CreditsText.visible = false
+	$transition.play("fade_out")

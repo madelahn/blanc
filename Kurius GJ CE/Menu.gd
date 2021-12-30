6412d3b30dac16d455008c228392329d9b90de34
Madeline <93456777+madelahn@users.noreply.github.com>
2021-12-29 19:20:16 -0500

    Add files via upload

--- Kurius GJ CE/Menu.gd
+++ Kurius GJ CE/Menu.gd
@@ -1,21 +1,15 @@
 extends Control
 
-onready var anim_clouds = $anim_clouds
-onready var anim_trees = $anim_trees
-onready var anim_mountains = $anim_mountains
-onready var anim_snow = $anim_snow
-onready var transition = $transition
-
 func _ready():
-	anim_clouds.play("Clouds")
-	anim_trees.play("Trees 2")
-	anim_mountains.play("mountains")
-	anim_snow.play("Flakes")
+	$anim_clouds.play("Clouds")
+	$anim_trees.play("Trees 2")
+	$anim_mountains.play("mountains")
+	$anim_snow.play("Flakes")
 
 
 func _on_StartButton_pressed():
 	$ButtonClickedSound.play()
-	transition.play("fade")
+	$transition.play("fade")
 	$fade_bgm.play("fade_out")
 	if $ButtonClickedSound.playing:
 		yield($ButtonClickedSound, "finished")
